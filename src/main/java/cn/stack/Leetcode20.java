f6c97589024811ebd37203564812f9df69c3c6ab
shuren mai <maisrcn@qq.com>
2021-06-24 20:56:48 +0800

    添加栈的题目，开始画图描述解法

---
+++ src/main/java/cn/stack/Leetcode20.java
@@ -0,0 +1,39 @@
+package cn.stack;
+
+import java.util.Stack;
+
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']'的字符串 s ，判断字符串是否有效。
+ * <p>
+ * 有效字符串需满足：
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * <p>
+ * 难度：简单
+ * https://leetcode-cn.com/problems/valid-parentheses/
+ */
+public class Leetcode20 {
+
+    public boolean isValid(String s) {
+        if (s.isEmpty()) {
+            return true;
+        }
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (stack.empty() || c != stack.pop()) {
+                return false;
+            }
+        }
+        return stack.empty();
+    }
+}
