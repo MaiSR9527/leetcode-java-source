4d295d15e820983120c0fa44d6b73b7232e0df53
srMai <maisrcn@qq.com>
2021-07-20 14:16:10 +0800

    提交代码

---
+++ src/main/java/cn/linklist/Leetcode234.java
@@ -0,0 +1,40 @@
+package cn.linklist;
+
+import cn.common.ListNode;
+
+public class Leetcode234 {
+
+
+    /**
+     * 快慢指针
+     *
+     * @param head
+     * @return
+     */
+    public boolean solution1(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        ListNode slow = head, fast = head;
+        ListNode pre = head, prepre = null;
+        while (fast != null && fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+            pre.next = prepre;
+            prepre = pre;
+        }
+        if (fast != null) {
+            slow = slow.next;
+        }
+        while (pre != null && slow != null) {
+            if (pre.val != slow.val) {
+                return false;
+            }
+            pre = pre.next;
+            slow = slow.next;
+        }
+        return true;
+
+    }
+}
---
+++ src/main/java/cn/stack/Leetcode155.java
@@ -0,0 +1,56 @@
+package cn.stack;
+
+/**
+ * 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+ * <p>
+ * push(x) —— 将元素 x 推入栈中。
+ * pop()—— 删除栈顶的元素。
+ * top()—— 获取栈顶元素。
+ * getMin() —— 检索栈中的最小元素。
+ * <p>
+ * 难度：简单
+ * https://leetcode-cn.com/problems/min-stack/
+ */
+public class Leetcode155 {
+    private Node head;
+
+    public Leetcode155() {
+    }
+
+    public void push(int val) {
+        if (head == null) {
+            head = new Node(val, val);
+        } else {
+            head = new Node(val, Math.min(val, head.min), head);
+        }
+    }
+
+    public void pop() {
+        head = head.next;
+    }
+
+    public int top() {
+        return head.val;
+    }
+
+    public int getMin() {
+        return head.min;
+    }
+
+    private class Node {
+        int val;
+        int min;
+        Node next;
+
+        private Node(int val, int min) {
+            this(val, min, null);
+        }
+
+        private Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+}
+
