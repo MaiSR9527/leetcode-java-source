061d824de1be8f93ba2f643eb55def5e2d224e08
maishuren <maisrcn@qq.com>
2022-09-26 23:35:32 +0800

    剑指offer：旋转数组的最小数字(modified)

--- src/main/java/cn/offer/Offer6.java
+++ src/main/java/cn/offer/Offer6.java
@@ -21,10 +21,6 @@ public class Offer6 {
         System.out.println(Offer6.minInReversingList(arr1));
         System.out.println(Offer6.minInReversingList(arr2));
         System.out.println(Offer6.minInReversingList(arr3));
-        System.out.println("=================");
-        System.out.println(Offer6.minInReversingListV2(arr1));
-        System.out.println(Offer6.minInReversingListV2(arr2));
-        System.out.println(Offer6.minInReversingListV2(arr3));
     }
 
     /**
@@ -36,34 +32,14 @@ public class Offer6 {
     public static int minInReversingList(int[] arr) {
         int left = 0;
         int right = arr.length - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (arr[mid] > arr[mid + 1]) {
-                return arr[mid + 1];
-            }
-            if (arr[mid - 1] > arr[mid]) {
-                return arr[mid];
-            }
-            if (arr[mid] > arr[0]) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return -1;
-    }
-
-    public static int minInReversingListV2(int[] arr) {
-        int left = 0;
-        int right = arr.length - 1;
         while (left < right) {
-            int mid = (right + left) / 2;
-            if (arr[mid] < arr[right]) {
-                right = mid;
-            } else if (arr[mid] > arr[right]) {
+            int mid = (left + right) / 2;
+            if (arr[mid] > arr[right]) {
                 left = mid + 1;
+            } else if (arr[mid] < arr[right]) {
+                right = mid;
             } else {
-                right -= 1;
+                right--;
             }
         }
         return arr[left];
