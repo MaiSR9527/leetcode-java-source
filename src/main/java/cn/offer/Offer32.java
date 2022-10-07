316d5094fbbbee61ac6cfe2ff0640fed28b66575
maishuren <maisrcn@qq.com>
2022-10-07 23:52:38 +0800

    剑指offer：从上到下打印二叉树

---
+++ src/main/java/cn/offer/Offer32.java
@@ -0,0 +1,58 @@
+package cn.offer;
+
+import cn.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 题目： 从上到下打印二叉树
+ * 从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
+ * <p>
+ * 例如:
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回：
+ * <p>
+ * [3,9,20,15,7]
+ * <p>
+ * 提示：
+ * 节点总数 <= 1000
+ * <p>
+ * 链接：https://leetcode.cn/problems/cong-shang-dao-xia-da-yin-er-cha-shu-lcof
+ *
+ * @author MaiShuRen
+ * @site https://www.maishuren.top
+ * @since 2022-10-07 23:17
+ **/
+public class Offer32 {
+
+    public int[] levelOrder(TreeNode root) {
+        if (root == null) {
+            return new int[0];
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        List<Integer> result = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            result.add(node.val);
+            if (node.left!=null) {
+                queue.add(node.left);
+            }
+            if (node.right!=null) {
+                queue.add(node.right);
+            }
+        }
+        int[] ans = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            ans[i] = result.get(i);
+        }
+        return ans;
+    }
+}
