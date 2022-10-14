9606883148cae79b05cde0e4389064bba8185746
maishuren <maisrcn@qq.com>
2022-10-14 23:27:51 +0800

    剑指offer：1～n 整数中 1 出现的次数

---
+++ src/main/java/cn/offer/Offer43.java
@@ -0,0 +1,45 @@
+package cn.offer;
+
+/**
+ * 题目：1～n 整数中 1 出现的次数
+ * 输入一个整数 n ，求1～n这n个整数的十进制表示中1出现的次数。
+ * <p>
+ * 例如，输入12，1～12这些整数中包含1 的数字有1、10、11和12，1一共出现了5次。
+ * <p>
+ * 示例 1：
+ * 输入：n = 12
+ * 输出：5
+ * <p>
+ * 示例 2：
+ * 输入：n = 13
+ * 输出：6
+ * <p>
+ * 限制：
+ * 1 <= n < 2^31
+ * <p>
+ * 链接：https://leetcode.cn/problems/1nzheng-shu-zhong-1chu-xian-de-ci-shu-lcof
+ *
+ * @author MaiShuRen
+ * @site https://www.maishuren.top
+ * @since 2022-10-14 23:11
+ **/
+public class Offer43 {
+    public int countDigitOne(int n) {
+        int digit = 1, res = 0;
+        int high = n / 10, cur = n % 10, low = 0;
+        while (high != 0 || cur != 0) {
+            if (cur == 0) {
+                res += high * digit;
+            } else if (cur == 1) {
+                res += high * digit + low + 1;
+            } else {
+                res += (high + 1) * digit;
+            }
+            low += cur * digit;
+            cur = high % 10;
+            high /= 10;
+            digit *= 10;
+        }
+        return res;
+    }
+}
